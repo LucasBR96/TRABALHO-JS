0db8b0ab7267b3e09920f52dcea2e2a5dcd0189d
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-09-14 10:49:59 -0300

    trapalhada resolvida

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Função auxiliar ---------------------------------------------------------------
-function file_into_string( path ){
-	return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut aliquam sapien. Quisque eros tortor, bibendum et tempor eget, laoreet at risus. Suspendisse commodo massa orci, at ultricies tellus lobortis vel. Aliquam bibendum sagittis leo ac pellentesque. Duis gravida lorem et ligula pharetra, in eleifend diam euismod. Cras dolor odio, scelerisque iaculis ipsum a, accumsan porta dui. Etiam at nulla sodales, luctus arcu ut, pretium orci. Maecenas eleifend suscipit justo vel luctus. Sed vitae augue orci. Morbi ipsum quam, rhoncus ac laoreet non, dapibus id massa. Cras congue, risus eget sollicitudin iaculis, ipsum justo tristique odio, convallis placerat libero enim eget tellus. Nullam eu faucibus tellus.";
-}
 // Constructor Function ----------------------------------------------------------
 
-function Culinaria ( nome, desc, foto, pais ){
+function Receita ( nome, desc, foto, pais ){
 	
 	this.foto = foto;
 	this.nome = nome;
@@ -15,7 +11,30 @@ function Culinaria ( nome, desc, foto, pais ){
 	
 	this.get_card = function(){
 		
-		let s = ``;
+		let s = `
+		<div class = "col-md-6 col-12 my-2">
+			<div class = "card text-center border-primary" style = "height: 100%">
+				<div class = "card-header text-danger" style = "background-color: rgb( 255 , 255 , 255 )">
+					<h4>${ this.nome }</h4>
+					<h5 class = "cul" >${ this.pais }</h5>
+
+				</div>
+				<div class = "card-body pb-0">
+					<img src = "${ this.foto }"
+					style = "width: 100%; height : 80%">
+					<p class = "text-justify-center mt-3">
+						${ this.desc }
+					</p>
+				</div>
+				<div class = "card-footer" style = "background-color: rgb( 255 , 255 , 255 )" >
+					<button type = "button" 
+					class = "btn"
+					style = "color :rgb(0, 50, 160) ; border-color: rgb(0, 50, 160)">
+						confira </button>
+				</div>
+			</div>
+		</div>
+		`
 		return s;
 	}
 }
@@ -23,7 +42,7 @@ function Culinaria ( nome, desc, foto, pais ){
 let to_cularr = function( lst ){
 	let result = [];
 	for( let item of lst )
-		result.push( new Culinaria( item.nome, item.desc, item.foto , item.pais) );
+		result.push( new Receita( item.nome, item.desc, item.foto , item.pais) );
 	return result.map( m => m.get_card() );
 }
 
@@ -38,10 +57,19 @@ $( document ).ready( function(){
 	// Lista de objetos --------------------------------------------------------------
 	
 	let culinaria_arr = [
-		{nome:"Russia", foto:"assets/russia/thumbnail.png", desc:"assets/russia/Intro.txt"},
-		{nome:"Armênia", foto:"assets/armenia/thumbnail.png", desc:"assets/armenia/Intro.txt"},
-		{nome:"Hungria", foto:"assets/hungria/thumbnail.png", desc:"assets/hungria/Intro.txt"},
-		{nome:"Polônia", foto:"assets/polonia/thumbnail.png", desc:"assets/polonia/Intro.txt"}
+		{
+			nome:"Borscht" , pais:"Russia", foto:"assets/russia/thumbnail.png",     
+			desc:"Uma tradicional sopa russa de beterraba, feita com um caldo de costela caseiro e servida com <i>Smetana<\/i>."
+		},{
+			nome:"Tan apur" , pais:"Armênia", foto:"assets/armenia/thumbnail.png",  
+			desc:"Um caldo feito a base de Iogurte natural. Leva trigo em grão, menta e cebola. Uma boa pedida vegetariana."
+		},{
+			nome:"Paprikash" , pais:"Hungria", foto:"assets/hungria/thumbnail.png", 
+			desc:"Cortes de frango assado sob um molho de paprica picante. Acompanha <i>Gnocci</i> italiano."
+		},{
+			nome:"Zurek" , pais:"Polônia", foto:"assets/polonia/thumbnail.png",     
+			desc:"Essa sopa é semelhante à japonesa <i>Miso<\/i> ao usar uma pasta de grão fermentada como base. Invés de soja, a polonesa usa centeio, conferindo leve acidez e cremosidade."
+		}
 	];
 	
 	let rec_arr = to_cularr( culinaria_arr );
@@ -51,5 +79,5 @@ $( document ).ready( function(){
 	const div = merge_cards( rec_arr );
 	// console.log( div );
 	
-	$("body").append( div );
+	$("#receitas-cards").append( div );
 } )
